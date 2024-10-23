32e6b3fc214a62e75a384caca76df033981887d2
Olga_N <nazar.olgaa@gmail.com>
2024-10-23 15:43:24 -0400

    Initial commit

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import { ENV_VARS } from "../config/envVars.js";
+
+export const generateTokenAndSetCookie = (userId, res) => {
+    const token = jwt.sign({userId}, ENV_VARS.JWT_SECRET, {expiresIn: "15d"});
+    res.cookie("token", token, {
+        maxAge: 15*24*60*60*1000, // in 15 days in MS
+        httpOnly: true, // prevent XSS attacks 
+        sameSite: "strict", //prevent CSRF attacks 
+        secure: ENV_VARS.NODE_ENV !== "development",
+    });
+    return token;
+};
