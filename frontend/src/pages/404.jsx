f417a9b691b507f38c34c917965ab1dce77a787b
liacoding <nazar.olgaa@gmail.com>
2024-11-08 15:19:53 -0500

    Hero section fixed

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
 	return (
 		<div
-			className='min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white'
+			className='bg-cover bg-center flex flex-col justify-center items-center text-white'
 			style={{ backgroundImage: `url('/404.png')` }}
 		>
 			<header className='absolute top-0 left-0 p-4 bg-black w-full '>
@@ -17,7 +17,7 @@ const NotFoundPage = () => {
 					Sorry, we can't find that page. You'll find lots to explore on the home page.
 				</p>
 				<Link to={"/"} className='bg-white text-black py-2 px-4 rounded'>
-					Netflix Home
+					Home
 				</Link>
 			</main>
 		</div>
